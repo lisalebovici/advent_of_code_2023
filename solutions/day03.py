981129cb3dc1ed11f2720e017033a83f51fd131f
Lisa Lebovici <lrlebovici@gmail.com>
2023-12-05 22:37:42 -0500

    3-2 solution

--- solutions/day03.py
+++ solutions/day03.py
@@ -1,5 +1,50 @@
 import re
 
+
+def check_left_and_right(substring, left=True):
+    pattern = r'([0-9]+)$' if left else r'^([0-9]+)'
+    try:
+        num = re.search(pattern, substring).group(1)
+        return int(num)
+    except AttributeError:
+        return None
+
+
+def check_above_and_below(row, idx, numbers):
+    left_check = False
+    right_check = False
+    left_idx = idx - 1
+    right_idx = idx + 1
+    num = row[idx]
+    while (not left_check or not right_check):
+        if not left_check:
+            try:
+                left_char = row[left_idx]
+                if left_char in numbers:
+                    num = left_char + num
+                    left_idx -= 1
+                else:
+                    left_check = True
+            except IndexError:
+                left_check = True
+        if not right_check:
+            try:
+                right_char = row[right_idx]
+                if right_char in numbers:
+                    num = num + right_char
+                    right_idx += 1
+                else:
+                    right_check = True
+            except IndexError:
+                right_check = True
+    return int(num)
+
+
+def check_diagonals(row, idx, left=True):
+    diag = row[max(idx-3,0):idx] if left else row[idx+1:min(idx+4,len(row))]
+    return check_left_and_right(diag, left)
+
+
 def main():
     # schematic = [
     #     '467..114..',
@@ -20,13 +65,12 @@ def main():
     checks = []
     numbers = [str(x) for x in range(10)]
     nonsymbols = numbers + ['.']
-
     nrow = len(schematic)
     ncol = len(schematic[0])
+    print(nrow)
+    print(ncol)
 
-    # print(nrow)
-    # print(ncol)
-
+    # Part 1
     for i, row in enumerate(schematic):
         row_check = ''
         for j, char in enumerate(row):
@@ -63,6 +107,71 @@ def main():
 
     print(part_numbers)
     print(sum(part_numbers))
+
+    # Part 2
+    gear_ratios = []
+    for i, row in enumerate(schematic):
+        if '*' not in row:
+            continue
+        idxs = [j for j, char in enumerate(row) if char == '*']
+        for j in idxs:
+            adjacent_parts = 0
+            adjacent_numbers = []
+            # look left and right
+            left = check_left_and_right(row[:j], True)
+            if left:
+                adjacent_parts += 1
+                adjacent_numbers.append(left)
+            right = check_left_and_right(row[j+1:], False)
+            if right:
+                adjacent_parts += 1
+                adjacent_numbers.append(right)
+            # look above and below
+            if i > 0:
+                above = schematic[i - 1]
+                # first look directly above
+                if above[j] in numbers:
+                    num = check_above_and_below(above, j, numbers)
+                    adjacent_parts += 1
+                    adjacent_numbers.append(num)
+                # otherwise check diagonals
+                else:
+                    # check left diagonal
+                    left_diag = check_diagonals(above, j, True)
+                    if left_diag:
+                        adjacent_parts += 1
+                        adjacent_numbers.append(left_diag)
+                    # check right diagonal
+                    right_diag = check_diagonals(above, j, False)
+                    if right_diag:
+                        adjacent_parts += 1
+                        adjacent_numbers.append(right_diag)
+            if i < nrow - 1:
+                below = schematic[i + 1]
+                # first look directly below
+                if below[j] in numbers:
+                    num = check_above_and_below(below, j, numbers)
+                    adjacent_parts += 1
+                    adjacent_numbers.append(num)
+                # otherwise check diagonals
+                else:
+                    # check left diagonal
+                    left_diag = check_diagonals(below, j, True)
+                    if left_diag:
+                        adjacent_parts += 1
+                        adjacent_numbers.append(left_diag)
+                    # check right diagonal
+                    right_diag = check_diagonals(below, j, False)
+                    if right_diag:
+                        adjacent_parts += 1
+                        adjacent_numbers.append(right_diag)
+            if adjacent_parts == 2:
+                gear_ratios.append(adjacent_numbers[0] * adjacent_numbers[1])
+
+    print(gear_ratios)
+    print(len(gear_ratios))
+    print(sum(gear_ratios))
+
 
 if __name__ == '__main__':
     main()
