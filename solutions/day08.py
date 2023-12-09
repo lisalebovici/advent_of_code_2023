effc4dd53ec01a7c63c5f1e59b7de8427829f266
Lisa Lebovici <lrlebovici@gmail.com>
2023-12-09 17:04:22 -0500

    8-2 solution

--- solutions/day08.py
+++ solutions/day08.py
@@ -1,4 +1,4 @@
-import re
+import math
 
 def main():
     # maps = [
@@ -12,6 +12,18 @@ def main():
     #     'GGG = (GGG, GGG)',
     #     'ZZZ = (ZZZ, ZZZ)'
     # ]
+    # maps2 = [
+    #     'LR',
+    #     '',
+    #     '11A = (11B, XXX)',
+    #     '11B = (XXX, 11Z)',
+    #     '11Z = (11B, XXX)',
+    #     '22A = (22B, XXX)',
+    #     '22B = (22C, 22C)',
+    #     '22C = (22Z, 22Z)',
+    #     '22Z = (22B, 22B)',
+    #     'XXX = (XXX, XXX)',
+    # ]
     with open('../data/day08.txt', 'r') as file:
         maps = file.read().splitlines()
 
@@ -19,17 +31,44 @@ def main():
     network = [m.split(' = ') for m in maps[2:]]
     d_network = {n[0]: n[1].strip('()').split(', ') for n in network}
 
+    # Part 1
     not_at_zzz = True
     current_node = 'AAA'
     num_steps = 0
     while not_at_zzz:
-        for instruc in instructions:
-            current_node = d_network[current_node][int(instruc)]
+        # "repeat the whole sequence of instructions as necessary"
+        for i in instructions:
+            current_node = d_network[current_node][int(i)]
             num_steps += 1
-            if current_node == 'ZZZ':
-                not_at_zzz = False
-                break
+        # then check if we're at the end node
+        if current_node == 'ZZZ':
+            not_at_zzz = False
+            break
     print(f'Part 1: {num_steps}')
+
+    # Part 2
+    a_nodes = [node for node in d_network.keys() if node[-1] == 'A']
+    z_nodes = [node for node in d_network.keys() if node[-1] == 'Z']
+    current_nodes = a_nodes
+    iterations = []
+    for current_node in current_nodes:
+        print(current_node)
+        num_steps2 = 0
+        not_at_zzz2 = True
+        while not_at_zzz2:
+            # "repeat the whole sequence of instructions as necessary"
+            for i in instructions:
+                current_node = d_network[current_node][int(i)]
+                num_steps2 += 1
+            # then check if we're at one of the end nodes
+            if current_node in z_nodes:
+                print(current_node)
+                not_at_zzz2 = False
+        iters = int(num_steps2 / len(instructions))
+        iterations.append(iters)
+        print(f'iterations: {iters}')
+    num_steps2_total = math.lcm(*iterations) * len(instructions)
+    print(f'Part 2: {num_steps2_total}')
 
 if __name__ == '__main__':
     main()
